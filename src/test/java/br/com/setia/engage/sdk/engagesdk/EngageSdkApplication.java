fd04355485f1fb7767ad71f2b1e5752a2e5068bc
Sergio Miguel <sergiobmiguel@msn.com>
2019-12-20 17:16:49 -0300

    initial setup

---
+++ src/test/java/br/com/setia/engage/sdk/engagesdk/EngageSdkApplication.java
@@ -0,0 +1,13 @@
+package br.com.setia.engage.sdk.engagesdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngageSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngageSdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/setia/engage/sdk/engagesdk/EngageSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.setia.engage.sdk.engagesdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngageSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
